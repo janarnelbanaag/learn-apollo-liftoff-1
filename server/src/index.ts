d74fbb1036069d47e7056e865ef2f00f72af3037
Jan Arnel R. Banaag <58939777+janarnelbanaag@users.noreply.github.com>
2025-05-09 11:57:48 +0800

    initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-//TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+
+  console.log(`Server is running at ${url}`);
+}
+
+startApolloServer();
